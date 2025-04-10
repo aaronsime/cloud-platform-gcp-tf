b4fda1595ced8b12c20e9cba13f27749ff10c833
aaron <aaronsime@hotmail.co.nz>
2025-04-10 12:56:54 +1200

    adding addtional job for transform

--- pubsub.tf
+++ pubsub.tf
@@ -2,6 +2,6 @@ resource "google_pubsub_topic" "cloud_scheduler_topic" {
   name = "cloud-scheduler-topic"
 }
 
-resource "google_pubsub_topic" "cloud_scheduler_topic" {
+resource "google_pubsub_topic" "cloud_scheduler_transform_topic" {
   name = "cloud-scheduler-transform-topic"
 }
