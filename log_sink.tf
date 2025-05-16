8a0627513e79a88a833c3f52546228cb41156bce
aaron <aaronsime@hotmail.co.nz>
2025-05-16 13:33:35 +1200

    adding pub/sub for n8n

--- log_sink.tf
+++ log_sink.tf
@@ -8,3 +8,12 @@ EOT
 
   unique_writer_identity = true
 }
+
+resource "google_pubsub_topic_iam_binding" "allow_sink_publish" {
+  topic = google_pubsub_topic.cloud_run_logs.name
+
+  role    = "roles/pubsub.publisher"
+  members = [
+    "serviceAccount:${google_logging_project_sink.cloud_run_error_sink.writer_identity}"
+  ]
+}
