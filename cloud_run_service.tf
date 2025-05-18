66e48174441f35f4095d4a379f41a6f494de2eba
aaron <aaronsime@hotmail.co.nz>
2025-05-18 20:04:46 +1200

    setting up service

---
+++ cloud_run_service.tf
@@ -0,0 +1,62 @@
+resource "google_pubsub_topic" "log_errors" {
+  name = "log-errors"
+}
+
+resource "google_cloud_run_service" "log_agent" {
+  name     = "log-agent"
+  location = var.region
+
+  template {
+    spec {
+      containers {
+        image = "${var.region}-docker.pkg.dev/${var.environment}-cloud-warehouse/cloud-warehouse-snowflake-${var.environment}/cloud-warehouse-snowflake-${var.environment}:latest"
+
+        env {
+          name  = "ENVIRONMENT"
+          value = var.environment
+        }
+        env {
+          name  = "PROJECT_ID"
+          value = var.project_id
+        }
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+
+resource "google_cloud_run_service_iam_member" "allow_pubsub" {
+  service  = google_cloud_run_service.log_agent.name
+  location = google_cloud_run_service.log_agent.location
+  role     = "roles/run.invoker"
+  member   = "serviceAccount:service-${var.project_number}@gcp-sa-pubsub.iam.gserviceaccount.com"
+}
+
+resource "google_eventarc_trigger" "log_agent_trigger" {
+  name     = "log-agent-trigger"
+  location = var.region
+
+  matching_criteria {
+    attribute = "type"
+    value     = "google.cloud.pubsub.topic.v1.messagePublished"
+  }
+
+  transport {
+    pubsub {
+      topic = google_pubsub_topic.log_errors.id
+    }
+  }
+
+  destination {
+    cloud_run_service {
+      service = google_cloud_run_service.log_agent.name
+      region  = var.region
+    }
+  }
+
+  service_account = google_service_account.cloud_sa.email
+}
