740a094e90fc1b23d7ebaa29a2ac9109e2644bf9
aaron <aaronsime@hotmail.co.nz>
2025-05-21 21:01:41 +1200

    refactoring

--- jobs.tf
+++ jobs.tf
@@ -1,5 +1,5 @@
 resource "google_cloud_run_v2_job" "bigquery_ingestion" {
-  name     = "cloud-scheduler-cloudrun-job-bigquery_ingestion"
+  name     = "cloud-scheduler-cloudrun-job-bigquery-ingestion"
   location = var.region
 
   labels = {
